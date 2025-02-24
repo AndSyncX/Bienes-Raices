98b87f5f59ae2eb133efc7e5e5ba965d8d0f6184
AndSyncX <fernandezp.joe@gmail.com>
2025-02-24 18:17:07 -0500

    Actualizaciones en PHP del CRUD

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -22,10 +22,10 @@
                     <img class="dark-mode-boton" src="/build/img/dark-mode.svg">
                     
                     <nav class="navegacion">
-                        <a href="nosotros.php">Nosotros</a>
-                        <a href="anuncios.php">Anuncios</a>
-                        <a href="blog.php">Blog</a>
-                        <a href="contacto.php">Contacto</a>
+                        <a href="/nosotros.php">Nosotros</a>
+                        <a href="/anuncios.php">Anuncios</a>
+                        <a href="/blog.php">Blog</a>
+                        <a href="/contacto.php">Contacto</a>
                     </nav>
                 </div>
             </div><!--.barra-->
